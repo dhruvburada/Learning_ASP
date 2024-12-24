eb4568978209790a0492eac2166b638c6e3fc4ab
Dhruv Burada <buradaddhruv35@gmail.com>
2024-12-24 11:00:12 +0530

    Add project files.

---
+++ WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Demo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Enter Num 1"></asp:Label>
+            <asp:TextBox ID="num1" runat="server"></asp:TextBox><br>
+            <asp:Label ID="Label2" runat="server" Text="Enter Num 2"></asp:Label>
+            <asp:TextBox ID="num2" runat="server"></asp:TextBox><br>
+            <asp:Button ID="Add" runat="server" Text="Button" OnClick="add" />
+            <asp:Label ID="output" runat="server" Text=""></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Demo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void add(object sender, EventArgs e)
+        {
+            try
+            {
+                // Retrieve values from the TextBox controls
+                int number1 = int.Parse(num1.Text);
+                int number2 = int.Parse(num2.Text);
+
+                output.Text = "Value Fethced";
+
+                // Calculate the sum
+                int sum = number1 + number2;
+
+                // Display the result in the Label control
+                output.Text = "The sum is: " + sum.ToString();
+            }
+            catch (FormatException)
+            {
+                // Handle invalid input
+                output.Text = "Please enter valid numbers!";
+            }
+        }
+    }
+}
